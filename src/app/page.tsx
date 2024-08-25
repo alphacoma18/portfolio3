0a77d11742eae41f445742c00240eddee2d69db9
Alpha Romer Coma <alpharomercoma@proton.me>
2024-08-25 18:57:14 +0000

    refactor: add projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Competencies from "@/components/Competencies";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Projects from "@/components/Projects";
 import Recommendations from "@/components/Recommendations";
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <main className="w-full">
         <Hero />
         <Competencies />
+        <Projects />
         <Recommendations />
       </main>
     </span>
