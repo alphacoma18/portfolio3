d286d54528dbd86bba2eb89a0efe5e6f6b57c96d
Alpha Romer Coma <alpharomercoma@proton.me>
2024-08-25 19:05:59 +0000

    refactor: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Navbar from "@/components/Navbar";
 import Projects from "@/components/Projects";
 import Recommendations from "@/components/Recommendations";
 import Affiliations from "@/components/Affiliations";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <span className="block md:flex w-full">
@@ -14,6 +15,7 @@ export default function Home() {
         <Projects />
         <Affiliations />
         <Recommendations />
+        <Footer />
       </main>
     </span>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="bg-black text-white py-8 w-full">
+            <div className="container mx-auto px-4">
+                <div className="text-center text-sm">
+                    &copy; 2024 Alpha Romer Coma. All rights reserved.
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
